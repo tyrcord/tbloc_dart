3e7a6090395c384bdfb06c1ced9f9836c42419ee
Thibault Zanini <thibaultzanini@gmail.com>
2020-09-18 16:30:04 +0200

    Add isInitialized and isInitializing flags to BlocState

--- lib/core/states/bloc_state.dart
+++ lib/core/states/bloc_state.dart
@@ -2,8 +2,17 @@ import 'package:equatable/equatable.dart';
 
 abstract class BlocState extends Equatable {
   final dynamic error;
+  final bool isInitializing;
+  final bool isInitialized;
 
   bool get hasError => error != null;
 
-  const BlocState({this.error});
+  const BlocState({
+    bool isInitializing,
+    bool isInitialized,
+    this.error,
+  })  : isInitializing = isInitializing ?? false,
+        isInitialized = isInitialized ?? false;
+
+  BlocState copyWith();
 }
--- lib/core/states/hydrated_state.dart
+++ lib/core/states/hydrated_state.dart
@@ -4,10 +4,16 @@ abstract class HydratedBlocState extends BlocState {
   final bool hydrated;
 
   const HydratedBlocState({
+    bool isInitializing,
+    bool isInitialized,
     bool hydrated,
     dynamic error,
   })  : hydrated = hydrated ?? false,
-        super(error: error);
+        super(
+          isInitializing: isInitializing,
+          isInitialized: isInitialized,
+          error: error,
+        );
 
   BlocState copyWith({bool hydrated});
 }
