bcff8603c9ee18fded8af288eed3d4aae835f2ac
Thibault Zanini <thibaultzanini@gmail.com>
2021-04-05 19:50:12 +0200

    clean up

--- lib/core/states/hydrated_state.dart
+++ lib/core/states/hydrated_state.dart
@@ -1,18 +1,18 @@
 import 'bloc_state.dart';
 
 abstract class HydratedBlocState extends BlocState {
-  @override
-  final bool isInitializing;
-  @override
-  final bool isInitialized;
   final bool hydrated;
 
   const HydratedBlocState({
-    this.isInitializing = false,
-    this.isInitialized = false,
+    bool isInitializing = false,
+    bool isInitialized = false,
     this.hydrated = false,
     dynamic? error,
-  }) : super(error: error);
+  }) : super(
+          error: error,
+          isInitializing: isInitializing,
+          isInitialized: isInitialized,
+        );
 
   @override
   BlocState copyWith({bool? hydrated});
